1039f2632a49b6ce3d838793e7990d768128ff2f
Jonathan Pares <jonathan.pares@gmail.com>
2012-06-30 18:55:45 +0200

    Initial commit.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Airinfos::Application.config.session_store :cookie_store, key: '_airinfos_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Airinfos::Application.config.session_store :active_record_store
