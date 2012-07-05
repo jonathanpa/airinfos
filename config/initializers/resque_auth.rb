33e577a9172d02382da0f512aedad5ca66cba15c
Jonathan Pares <jonathan.pares@gmail.com>
2012-07-05 17:08:52 +0200

    Removed credentials (don't try the old ones, I've changed all).

--- config/initializers/resque_auth.rb
+++ config/initializers/resque_auth.rb
@@ -1,3 +1,3 @@
 Resque::Server.use(Rack::Auth::Basic) do |username, password|
-  [username, password] == ["PeanutsTheBear", "CEOofRedate"]
+  [username, password] == [ENV['RESQUE_USER'], ENV['RESQUE_PASSWORD']]
 end
