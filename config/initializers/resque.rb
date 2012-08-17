c5345788dc7d2da13d8b98deb063cf5b7351a63b
Jonathan Pares <jonathan.pares@gmail.com>
2012-08-17 19:39:39 +0200

    Clean redistodo env key and moved to local redis instance.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,9 +1,5 @@
 if Rails.env == 'development'
-  ENV['REDISTOGO_URL'] = "redis://jonathanpa:b1d8b882abcc8bf77c571ff98d1ece8c@lab.redistogo.com:9557"
-
-  uri = URI.parse(ENV['REDISTOGO_URL'])
-  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
-
+  Resque.redis = Redis.new(:host => 'localhost', :port => 6379, :thread_safe => true)
   Resque.redis.namespace = 'resque.Airinfos'
 
   Resque.schedule = YAML.load_file(Rails.root.join('config', 'resque_schedule.yml'))
