be789a0cdbea6e0f541f2d2534f6c79300315cf2
Jonathan Pares <jonathan.pares@gmail.com>
2012-12-07 23:57:52 +0100

    Looked that I removed all was related to Resque.

--- config/initializers/resque.rb
+++
@@ -1,6 +0,0 @@
-if Rails.env == 'development'
-  Resque.redis = Redis.new(:host => 'localhost', :port => 6379, :thread_safe => true)
-  Resque.redis.namespace = 'resque.Airinfos'
-
-  Resque.schedule = YAML.load_file(Rails.root.join('config', 'resque_schedule.yml'))
-end
