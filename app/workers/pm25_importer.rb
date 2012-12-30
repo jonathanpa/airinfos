f2abeb925b51ae6f1dda1a2b9891a142b14ebb0f
Jonathan Pares <jonathan.pares@gmail.com>
2012-12-30 23:42:46 +0100

    Calculate the measures with the average of the last hour.

--- app/workers/pm25_importer.rb
+++ app/workers/pm25_importer.rb
@@ -1,5 +1,4 @@
 class Pm25Importer
-  @queue = :imports_queue
 
   def self.perform
     mapping = { angers: [2], lemans: [3], nantes: [4, 5], saintnazaire: [6] }
@@ -8,9 +7,7 @@ class Pm25Importer
     time_measure = DateTime.civil(now.year, now.month, now.day, now.hour)
     yesterday = time_measure - 1.day
 
-    #xls_source = "tmp/mesures_29-06-2012.xls"
     xls_source = "http://www.airpl.org/mesuresindices/mesurexcel?polluant=P2&site[]=_BART&site[]=_SOUR&site[]=_BOUT&site[]=_VHUG&site[]=_BLUM&periode=_2&j=#{yesterday.day}&m=#{yesterday.month}&a=#{yesterday.year}&=excel" << ".xls"
-
 
     pdl_measures = Excel.new(xls_source)
     pdl_measures.default_sheet = pdl_measures.sheets.first
@@ -27,15 +24,24 @@ class Pm25Importer
   private
 
   def self.import_data_from_city(city_code, time_measure, xls, columns)
-    row = 101 + 4 * time_measure.hour
+    end_row = 101 + 4 * time_measure.hour
+    start_row = end_row - 4
+
     city = City.find_by_code(city_code.to_s)
     city_pm2s = []
 
     columns.each do |c|
-      pm2 = 0
-      pm2 = xls.cell(row, c) unless xls.empty?(row, c) || xls.cell(row, c).blank?
+      pm2_total = 0
+      pm2_count = 0
 
-      city_pm2s.push(pm2)
+      (start_row..end_row).each do |row|
+        unless xls.cell(row, c).blank?
+          pm2_total += xls.cell(row, c)
+          pm2_count += 1
+        end
+      end
+
+      pm2_total == 0 ? city_pm2s.push(0) : city_pm2s.push(pm2_total / pm2_count)
     end
 
     city_pm2s.sort!.reverse!
